32b61b8db7e2e32a09143916f61e4cbfbcd01145
Michel Barbosa <michelbarbosa@outlook.com>
2024-05-07 15:40:30 -0300

    Altera o texto do segundo paragrado

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// Seu código aqui
+const alteraTexto = () => {
+    let texto = document.getElementsByTagName('p')[1];
+    texto.innerText = 'Daqui a 2 anos estarei sendo promovido no Banco do Brasil'
+};
+
+alteraTexto();
